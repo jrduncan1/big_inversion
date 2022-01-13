09352ec6a23a9d9e51309e5b7c8e5d991bebf05e
jrduncan1 <dunc5201@gmail.com>
2022-01-12 20:30:16 -0700

    big inversion assignment

---
+++ src/components/PersonCard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const PersonCard = props => {
+    return(
+        <div>
+            <h1>{props.lastName}, {props.firstName}</h1>
+            <h4>Age: {props.age}</h4>
+            <h4>Hair Color: {props.hairColor}</h4>
+        </div>
+    );
+}
+
+export default PersonCard;
